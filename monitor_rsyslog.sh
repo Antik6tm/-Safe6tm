f139b976c9564d61a981ba1306ba91e8e3eba388
Antik6tm <6tmik@proton.me>
2025-02-27 08:31:31 +0100

    Update monitor_rsyslog.sh

--- monitor_rsyslog.sh
+++ monitor_rsyslog.sh
@@ -6,9 +6,10 @@ mkdir -p "$LOG_DIR"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Vérification des logs système." >> "$ALERT_LOG"
 
-grep -Ei "fail|error|critical|denied|unauthorized|intrusion|warning" /var/log/syslog >> "$ALERT_LOG"
-grep -Ei "fail|error|critical|denied|unauthorized" /var/log/auth.log >> "$ALERT_LOG"
-grep -Ei "fail|error|critical|denied|unauthorized" /var/log/kern.log >> "$ALERT_LOG"
+# Vérification de l'existence des logs avant de les scanner
+[[ -f "/var/log/syslog" ]] && grep -Ei "fail|error|critical|denied|unauthorized|intrusion|warning" /var/log/syslog >> "$ALERT_LOG"
+[[ -f "/var/log/auth.log" ]] && grep -Ei "fail|error|critical|denied|unauthorized" /var/log/auth.log >> "$ALERT_LOG"
+[[ -f "/var/log/kern.log" ]] && grep -Ei "fail|error|critical|denied|unauthorized" /var/log/kern.log >> "$ALERT_LOG"
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Surveillance des logs système terminée." >> "$ALERT_LOG"
 exit 0
